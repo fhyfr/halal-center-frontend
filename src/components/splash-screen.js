1487ff853adbeab48adf9f2427bcfb3ce8fd56ee
Firman <fhaya.firman@gmail.com>
2024-08-06 08:21:30 +0700

    chore: update @mui/styles to @mui/material/styles (#20)
    Co-authored-by: fhyfirman <firman@hungryhub.com>

--- src/components/splash-screen.js
+++ src/components/splash-screen.js
@@ -1,33 +1,30 @@
 import React from 'react';
-import { Box, LinearProgress } from '@mui/material';
-import { makeStyles } from '@mui/styles';
+import Box from '@mui/material/Box';
+import LinearProgress from '@mui/material/LinearProgress';
+import { styled } from '@mui/material/styles';
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    alignItems: 'center',
-    backgroundColor: theme.palette.background.default,
-    display: 'flex',
-    flexDirection: 'column',
-    height: '100%',
-    justifyContent: 'center',
-    left: 0,
-    padding: theme.spacing(3),
-    position: 'fixed',
-    top: 0,
-    width: '100%',
-    zIndex: 2000,
-  },
-}));
+const SlashScreenRoot = styled('div')({
+  alignItems: 'center',
+  backgroundColor: (theme) => theme.palette.background.default,
+  display: 'flex',
+  flexDirection: 'column',
+  height: '100%',
+  justifyContent: 'center',
+  left: 0,
+  padding: (theme) => theme.spacing(3),
+  position: 'fixed',
+  top: 0,
+  width: '100%',
+  zIndex: 2000,
+});
 
 const SlashScreen = () => {
-  const classes = useStyles();
-
   return (
-    <div className={classes.root}>
+    <SlashScreenRoot>
       <Box width={400}>
         <LinearProgress />
       </Box>
-    </div>
+    </SlashScreenRoot>
   );
 };
 
